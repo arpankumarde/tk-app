e827c02109368d1eb17fdd167a45b8ac02712979
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2026-03-30 08:31:34 +0530

    feat: add microsoft clarity analytics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,16 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
+import * as Clarity from "@microsoft/react-native-clarity";
+import Constants, { ExecutionEnvironment } from "expo-constants";
+
+if (Constants.executionEnvironment !== ExecutionEnvironment.StoreClient) {
+  try {
+    Clarity.initialize(process.env.EXPO_PUBLIC_CLARITY_ID!);
+  } catch (e) {
+    console.warn("Clarity failed to initialize:", e);
+  }
+}
 
 export default function RootLayout() {
   return (
